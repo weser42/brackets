335e98b734cb124559ec623ab3c27b564477b0cf
weser42 <58133977+weser42@users.noreply.github.com>
2022-02-16 22:26:18 +0400

    feat: added a solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let a = str.length;
+  for (let i in str) {
+    if (a > 1) {
+      for (let j = 0; j < a; j++) {
+        for (let k of bracketsConfig) {
+          if (str[j] == k[0]) {
+            if (str[j + 1] == k[1]) {
+              str = str.slice(0, j) + str.slice(j + 2);
+            }
+          }
+        }
+      }
+    }
+  }
+  return str == "" ? true : false;
+};
